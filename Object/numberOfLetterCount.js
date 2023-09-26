d97f3169afda9eaf2471bc7fe41c712dbd7bbf94
Vishal Ghag <vishal.ghag@i2econsulting.com>
2023-09-26 12:14:11 +0530

    program added to obj

---
+++ Object/numberOfLetterCount.js
@@ -0,0 +1,31 @@
+// wap to count the letter in string 
+
+// Input string
+const str = "Vishal Ghag";
+
+// Function to count the letters in a string and remove extra spaces
+const countString = (str) => {
+    // Initialize an empty object to store letter counts
+    let obj = {};
+
+    // Remove extra spaces and convert the string to lowercase
+    let data = str.toLowerCase().split(" ").join("");
+
+    // Iterate through each character in the cleaned string
+    for (let key of data) {
+        // Check if the character is already in the object
+        if (obj[key] > 0) {
+            // If yes, increment the count
+            obj[key]++;
+        } else {
+            // If no, initialize the count to 1
+            obj[key] = 1;
+        }
+    }
+
+    // Return the object containing letter counts
+    return obj;
+}
+
+// Call the function and log the result
+console.log(countString(str));
