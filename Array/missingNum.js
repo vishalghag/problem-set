a5d423fe675124b82d46d2fb7f90a2925daa7c65
Vishal Ghag <vishal.ghag@i2econsulting.com>
2023-09-28 12:02:18 +0530

    new code added in  array

---
+++ Array/missingNum.js
@@ -0,0 +1,28 @@
+// wap for finding out which number is missing in array
+
+// Input array of numbers
+let nums = [4, 3, 2, 7, 8, 2, 3, 1];
+
+// Function to find missing numbers in the array
+const missingArray = (nums) => {
+    // Create a new set from the input array to efficiently check for presence
+    let newSet = new Set(nums);
+
+    // Initialize an empty result array to store missing numbers
+    let result = [];
+
+    // Iterate through numbers from 1 to the length of the input array
+    for (let i = 1; i <= nums.length; i++) {
+        // Check if the number 'i' is not present in the set
+        if (!newSet.has(i)) {
+            // If 'i' is missing, push it to the result array
+            result.push(i);
+        }
+    }
+
+    // Return the result array containing missing numbers
+    return result;
+}
+
+// Call the function to find missing numbers and log the result
+console.log(missingArray(nums));
